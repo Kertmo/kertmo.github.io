96b0dd06a464bbd774fd4761deb45c64514955a8
Kertmo <kert.moist@ut.ee>
2026-04-27 22:35:22 +0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-let map = L.map("map").setView([58.373523, 26.716045], 12);
+let map = L.map("map").setView([58.373523, 26.716045], 12)
 
 const osm = L.tileLayer(
   "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
@@ -6,7 +6,7 @@ const osm = L.tileLayer(
     maxZoom: 19,
     attribution: "© OpenStreetMap contributors",
   }
-);
+)
 
 osm.addTo(map)
 
@@ -25,4 +25,4 @@ async function addDistrictsGeoJson(url) {
   polygons.addTo(map)
 }
 
-addDistrictsGeoJson('geojson/tartu_city_districts_edu.geojson')
+addDistrictsGeoJson("geojson/tartu_city_districts_edu.geojson")
