bbb928d89c1738e158cfad378baa16779e3c5c6b
Kertmo <kert.moist@ut.ee>
2026-04-29 22:46:36 +0300

    Update choroplethmap.js

--- choroplethmap/choroplethmap.js
+++ choroplethmap/choroplethmap.js
@@ -14,8 +14,8 @@ async function addGeoJson(url) {
   const response = await fetch(url)
   const data = await response.json()
   L.choropleth(data, {
-    valueProperty: 'OBJECTID',
-    scale: ['#ffffff', '#ff9900'],
+    valueProperty: 'TOWERS',
+    scale: ['#ffffff', '#0066cc'],
     steps: 5,
     mode: 'q', // q for quantile, e for equidistant
     style: {
