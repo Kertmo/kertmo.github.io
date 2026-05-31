ef5d4ee77e939fca0087a063dd97b955545731e3
Kertmo <kert.moist@ut.ee>
2026-05-31 11:57:09 +0300

    Update appCode.js

--- map2/js/appCode.js
+++ map2/js/appCode.js
@@ -11,6 +11,16 @@ const osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png
   attribution: 'OpenStreetMap contributors'
 });
 
+const satelliteLayer = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+  attribution: 'Esri, Maxar, Earthstar Geographics, and the GIS community',
+  maxZoom: 19
+});
+
+const topoLayer = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+  maxZoom: 17,
+  attribution: 'Map data: &copy; OpenStreetMap contributors, SRTM | Map style: &copy; OpenTopoMap (CC-BY-SA)'
+});
+
 // Default map settings
 function defaultMapSettings() {
   map.setView([58.373523, 26.716045], 12)
