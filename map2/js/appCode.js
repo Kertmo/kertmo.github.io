306f54f1b92405f55c7d09a2affb406f907493a5
Kertmo <kert.moist@ut.ee>
2026-05-31 15:08:47 +0300

    Update appCode.js

--- map2/js/appCode.js
+++ map2/js/appCode.js
@@ -1,8 +1,12 @@
+
 let map = L.map('map', {
   center: [58.373523, 26.716045],
   zoom: 12,
   zoomControl: true // Enable default zoom control
 });
+
+map.createPane('customDistrictsPane');
+map.getPane('customDistrictsPane').style.zIndex = 390;
 
 map.zoomControl.setPosition('topright');
 
@@ -18,7 +22,7 @@ const satelliteLayer = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/
 
 const topoLayer = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
   maxZoom: 17,
-  attribution: 'Map data: &copy; OpenStreetMap contributors, SRTM | Map style: &copy; OpenTopoMap (CC-BY-SA)'
+  attribution: 'Map data: &amp;copy; OpenStreetMap contributors, SRTM | Map style: &amp;copy; OpenTopoMap (CC-BY-SA)'
 });
 
 // Default map settings
@@ -55,7 +59,8 @@ async function loadDistrictsLayer() {
       },
       onEachFeature: function(feature, layer) {
         layer.bindPopup(feature.properties.NIMI || 'District ' + feature.properties.OBJECTID);
-      }
+      },
+      pane: 'customDistrictsPane'
     });
   } catch (error) {
     console.error("Error loading districts data:", error);
@@ -92,7 +97,8 @@ async function loadChoroplethLayer() {
       },
       onEachFeature: function(feature, layer) {
         layer.bindPopup('Value: ' + feature.properties.OBJECTID);
-      }
+      },
+      pane: 'customDistrictsPane'
     });
   } catch (error) {
     console.error("Error loading choropleth data:", error);
@@ -183,8 +189,6 @@ async function initializeLayers() {
   osmLayer.addTo(map);
 
   districtsLayer.addTo(map);
-  heatMapLayer.addTo(map);
-  
-  console.log(map)
 }
+
 initializeLayers();
