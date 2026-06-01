ab15db319b8c2c6e3e804e6ce67fa5a1f8cef0fa
Kertmo <kert.moist@ut.ee>
2026-06-01 19:57:10 +0300

    Update turfPractice.js

--- map2/js/turfPractice.js
+++ map2/js/turfPractice.js
@@ -1,15 +1,14 @@
 function turfFunctions() {
-  console.log('This text is from a module')
-  alert('Hello from my module!')
-}
+  console.log('This text is from a module');
+  alert('Hello from my module!');
 
-// define point coordinates
-const pointCoords = [26.71552, 58.37393]
-// define a point
-const myPoint = turf.point(pointCoords)
-// convert the point to geoJSON object
-const geoJSON_point = L.geoJSON(myPoint)
-// add the geoJSON object to the map
-geoJSON_point.addTo(map)
+  const pointCoords = [26.71552, 58.37393];
+
+  const myPoint = turf.point(pointCoords);
+
+  const geoJSON_point = L.geoJSON(myPoint);
+
+  geoJSON_point.addTo(map);
+}
 
 export { turfFunctions };
