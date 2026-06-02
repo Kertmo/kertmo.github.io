dee8f07fd2ec72c6d6b942d3c59ac7f3c99e2474
Kertmo <kert.moist@ut.ee>
2026-06-02 22:58:18 +0300

    Update layers.js

--- map2/js/layers.js
+++ map2/js/layers.js
@@ -29,6 +29,21 @@ const wmsLayers = [
     projection: "EPSG:3301",
     zIndex: 290,
   },
+{
+  id: "10_minute_areas",
+  title: {
+    en: "10 minute coverage area",
+    ee: "10 minutiga kaetav ala",
+  },
+  url: "https://landscape-geoinformatics.ut.ee/geoserver/pa2023/wms?",
+  layers: "rpk_10min_ala_db",
+  type: "wms",
+  version: "1.1.1",
+  format: "image/png",
+  transparent: true,
+  projection: "EPSG:3301",
+  zIndex: 280,
+}
 ]
 
 export { wmsLayers }
